6491eb84740b7c7c34fa5cbfaf7d7e5f5477f49b
Giovanna <giovannad1clonius@gmail.com>
2023-05-11 20:28:19 -0300

    birthday corrigido

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -4,7 +4,7 @@ const CustomersSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
     cpf: joi.string().min(11).max(11).pattern(new RegExp('^[0-9]+$')).required(),
-    birthday: joi.string().required().date()
+    birthday: joi.date().required()
 });
 
 export default CustomersSchema;
