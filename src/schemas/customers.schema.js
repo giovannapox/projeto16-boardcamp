d9cb4b402da3c0dd66880115010dbc408b33a4d9
Giovanna <giovannad1clonius@gmail.com>
2023-05-11 20:07:00 -0300

    pattern no joi cpf

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 const CustomersSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().min(11).max(11).required(),
+    cpf: joi.string().min(11).max(11).pattern(new RegExp('^[0-9]+$')).required(),
     birthday: joi.string().required()
 });
 
